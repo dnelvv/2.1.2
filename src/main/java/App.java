1705ae090f2167922aa8e3c0f3539e3aa61cb6be
dnelvv <91218302+dnelvv@users.noreply.github.com>
2022-01-06 19:48:48 +0300

    commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,23 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
         System.out.println(bean.getMessage());
+        System.out.println(bean1.getMessage());
+        System.out.println(bean == bean1);
+
+
+
+        Cat bean2 = (Cat) applicationContext.getBean("cat");
+        Cat bean3 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(bean2.getName());
+        System.out.println(bean3.getName());
+        System.out.println(bean2 == bean3);
     }
 }
